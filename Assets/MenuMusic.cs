85531ceaaf8b97bca97d145933b35d578d2b2d18
jamnewb <104563879@student.swin.edu.au>
2025-10-05 22:11:32 +1100

    menu music file comments
    added comments to the menu music file

--- Assets/MenuMusic.cs
+++ Assets/MenuMusic.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 public class MenuMusic : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private bool stopPlaying = false;
+    private bool stopPlaying = false; // Bool to stop the execution of another soundtrack after one has already been started
     void Start()
     {
     }
-    IEnumerator soundCoroutine()
+    IEnumerator soundCoroutine() // Coroutine to stop the update method for 68 seconds
     {
-        stopPlaying = true;
-        yield return new WaitForSeconds(68f);
-        stopPlaying = false;
+        stopPlaying = true; // Set bool to true to indicate no other sounds should play
+        yield return new WaitForSeconds(68f); // Wait 68 seconds
+        stopPlaying = false; // Set bool to false to indicate another sound can play
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() // Update method getting called constantly
     {
-        if (!stopPlaying)
+        if (!stopPlaying) // If no other music is playing then
         {
-            SoundManager.PlaySound(SoundType.MENUMUSIC);
-            StartCoroutine(soundCoroutine());
+            SoundManager.PlaySound(SoundType.MENUMUSIC); // Play the menu music if it is not on cooldown
+            StartCoroutine(soundCoroutine()); // Start the cooldown for the menu music to ensure only 1 plays at a time
         }
     }
 }
