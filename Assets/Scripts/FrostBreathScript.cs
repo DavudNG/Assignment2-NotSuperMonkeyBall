b6e479a8884aec9338e5935bd194048a23e6d79b
julian-saric <105346220@student.swin.edu.au>
2025-10-05 20:53:59 +1100

    Frozen status + fixes
    Added:
    Frozen status to ball
    Status indicator to top left of screen
    Ball changes colour when frozen
    Fixed canvas size to have constant size
    fixed ui element placements

--- Assets/Scripts/FrostBreathScript.cs
+++ Assets/Scripts/FrostBreathScript.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class FrostBreathScript : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
-    {
+    {   
         //if ball enter collides with frost breath, freezes ball by checking the gameObject tag.
         if (collision.CompareTag("Ball"))
         {
             Ball ball = collision.GetComponent<Ball>();
             if (ball != null)
             {
-                ball.Freeze(1f);
+                if (ball.Isfrozen() == false)
+                {
+                    ball.Freeze(3f);
+                }   
             }
         }
     }
@@ -23,7 +26,10 @@ public class FrostBreathScript : MonoBehaviour
             Ball ball = collision.GetComponent<Ball>();
             if (ball != null)
             {
-                ball.Freeze(3f);
+                if (ball.Isfrozen() == false)
+                {
+                    ball.Freeze(3f);
+                }
             }
         }
     }
